c65ee9af42713951467dc69d369c5bbf8e4b7503
swyftos <98902001+swyftos@users.noreply.github.com>
2025-10-07 12:20:50 +0200

    Create 2.ino

---
+++ partie 3/2.ino
@@ -0,0 +1,34 @@
+
+#define PIN_LED_ROUGE 9
+#define PIN_LED_VERTE 10
+#define PIN_LED_BLEUE 11
+
+#define INTERRUPTEUR_ROUGE A1
+#define INTERRUPTEUR_VERT A2
+#define INTERRUPTEUR_BLEU A3
+
+void setup() {
+
+  pinMode(PIN_LED_ROUGE, OUTPUT);
+  pinMode(PIN_LED_VERTE, OUTPUT);
+  pinMode(PIN_LED_BLEUE, OUTPUT);
+
+ 
+  pinMode(INTERRUPTEUR_ROUGE, INPUT);
+  pinMode(INTERRUPTEUR_VERT, INPUT);
+  pinMode(INTERRUPTEUR_BLEU, INPUT);
+}
+
+void loop() {
+
+  int etatRouge = digitalRead(INTERRUPTEUR_ROUGE);
+  int etatVert = digitalRead(INTERRUPTEUR_VERT);
+  int etatBleu = digitalRead(INTERRUPTEUR_BLEU);
+
+
+  analogWrite(PIN_LED_ROUGE, 255 * etatRouge);
+  analogWrite(PIN_LED_VERTE, 255 * etatVert);
+  analogWrite(PIN_LED_BLEUE, 255 * etatBleu);
+
+  delay(20); 
+}
